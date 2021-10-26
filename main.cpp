7d6a32f3ed9eed5ba0ec4846bac2c3d02b50f759
Filippo Toffano <filippo.toffano@studenti.unipd.it>
2021-10-26 09:45:27 +0200

    Implementate funzioni di stampa e di calcolo della stringa 'longest' e 'shortest' contenuta nel vettore string_v

--- main.cpp
+++ main.cpp
@@ -9,6 +9,13 @@ using std::endl;
 using std::vector;
 using std::string;
 
+// Function Declaration:
+
+void print_vector(vector<string> vector_to_print);
+void print_vector(vector<int> vector_lenght);
+vector<int> string_lengths(vector<string> length_argoument_vector);
+string longest_string(vector<string> longest_string_vector);
+string shortest_string(vector<string> shortest_string_vector);
 
 int main()
 {
@@ -23,22 +30,22 @@ int main()
 
 	cout << "Vector of strings: \n";
 	// 1. write a function "print" that prints the vector of strings
-	// print(string_v);
+	print_vector(string_v);
 
 	// 2. write a function string_lengths returns a vector
 	//    with length of each string in argument
-	// vector<int> lengths = string_lengths(string_v);
+	vector<int> lengths = string_lengths(string_v);
 
 	cout << "Vector of lengths:\n";
-	// print(lengths);
+	print_vector(lengths);
 
 	// 3. write a function "longest_string" that
 	//    returns longest string in a vector of strings
-	// cout << "Longest string: " << longest_string(string_v) << endl;
+	cout << "Longest string: " << longest_string(string_v) << endl;
 
 	// 4. write a function "shortest_string" that
 	// returns the shortest string in a vector of strings
-	// cout << "Shortest string: " << shortest_string(string_v) << endl;
+	cout << "Shortest string: " << shortest_string(string_v) << endl;
 
 	// 5. write a function "alpha_first_string" that
 	// returns lexicographically first string in a vector of strings
@@ -50,3 +57,64 @@ int main()
 
 	return 0;
 }
+
+// Function definition:
+
+void print_vector(vector<string> vector_to_print){
+	cout<<"{";
+	for(uint i = 0; i < vector_to_print.size(); i++){
+		cout<<" "<<vector_to_print[i];
+	}
+	cout<<" }"<<endl;
+}
+
+void print_vector(vector<int> vector_lenght){
+	cout<<"{";
+	for(uint i = 0; i < vector_lenght.size(); i++){
+		cout<<" "<< vector_lenght[i];
+	}
+	cout<<" }"<<endl;	
+}
+
+vector<int> string_lengths(vector<string> length_argoument_vector){
+	vector<int> lengh_argoument;
+	for(uint i = 0; i < length_argoument_vector.size(); i++){
+		lengh_argoument.push_back(length_argoument_vector[i].size());
+	}
+	return lengh_argoument;
+}
+
+string longest_string(vector<string> longest_string_vector){
+	uint longest_string = longest_string_vector[0].size();
+	uint pos_longest = 0;
+
+	for(uint i = 0; i < longest_string_vector.size(); i++){
+		if(longest_string < longest_string_vector[i].size()){
+			pos_longest = i;
+			longest_string = longest_string_vector[i].size();
+		}
+	}
+
+	return longest_string_vector[pos_longest];
+}
+
+string shortest_string(vector<string> shortest_string_vector){
+	uint shortest_string = shortest_string_vector[0].size();
+	uint pos_shortest = 0;
+
+	for(uint i = 0; i < shortest_string_vector.size(); i++){
+		if(shortest_string > shortest_string_vector[i].size()){
+			pos_shortest = i;
+			shortest_string = shortest_string_vector[i].size();
+		}
+	}
+
+	return shortest_string_vector[pos_shortest];
+}
+
+
+
+
+
+
+
